778d94093e6456b9e4229a1bf1eaffdab91f1c5c
GowsikanRaj <gowsikan25@gmail.com>
2022-05-31 19:59:33 -0400

    Changed watchlist to fetch from deployed server

--- src/Components/App.js
+++ src/Components/App.js
@@ -109,21 +109,29 @@ const App = () => {
   }, [stock, range, interval]);
 
   const getWatchlist = async () => {
-    const { data } = await axios.get("http://localhost:3001/getWatchlist");
+    const { data } = await axios.get(
+      "https://stockchartgeneratortwelvedata.herokuapp.com/getWatchlist"
+    );
     setWatchlist(data);
   };
 
   const removeFromWatchlist = async (id) => {
-    await axios.post("http://localhost:3001/deleteStock", {
-      id: id,
-    });
+    await axios.post(
+      "https://stockchartgeneratortwelvedata.herokuapp.com/deleteStock",
+      {
+        id: id,
+      }
+    );
     getWatchlist();
   };
 
   const addToWatchlist = async (stock) => {
-    await axios.post("http://localhost:3001/addStock", {
-      Stock: stock,
-    });
+    await axios.post(
+      "https://stockchartgeneratortwelvedata.herokuapp.com/addStock",
+      {
+        Stock: stock,
+      }
+    );
     getWatchlist();
   };
 
